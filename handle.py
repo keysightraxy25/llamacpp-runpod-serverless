27e8d1d94af31b1ae2a43fc90832a9a3778397b1
Rohan Gomes <rohan.gomes@keysight.com>
2025-09-29 23:16:41 +0530

    Add function to list models

--- handle.py
+++ handle.py
@@ -1,5 +1,6 @@
 import json
 import os
+import glob
 
 import runpod
 from llama_cpp import Llama
@@ -12,7 +13,13 @@ args.setdefault("model_path", "/models/DeepSeek-R1-0528-Qwen3-8B-Q8_0.gguf")
 llm = Llama(**args)
 
 def handler(event):
+    """
+    This function is the entry point for the serverless handler.
+    """
     inp = event.get("input", {}) or {}
+
+    if "list_models" in inp:
+        return str(glob.glob(os.path.join("/models", "*.gguf")))
 
     inp.setdefault("max_tokens", 4096)
 
